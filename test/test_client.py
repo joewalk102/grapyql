76d5eb436ff139b6d154f4703ea01ad870d2b154
Joe Walker <joe_walker@baxter.com>
2022-11-23 12:51:05 -0800

    Cleanup: Dedupe default structure.

--- test/test_client.py
+++ test/test_client.py
@@ -2,83 +2,45 @@ from grapyql.client import GqlClient
 
 
 class TestGqlClientDictToQuery:
-    def test_single_query_var(self):
-        client = GqlClient("localhost")
-        query = client.dict_to_query(
-            query_struct={
-                "user": {
-                    "active": bool,
-                    "fname": str,
-                    "invoice": {
-                        "cost": str,
-                        "address": {
-                            "street1": str,
-                            "street2": str,
-                            "city": str,
-                            "state": str,
-                            "zipcode": int,
-                        },
-                        "items": {
-                            "display_name": str,
-                            "sku": str,
-                        },
-                    },
-                }
+    client = GqlClient("localhost")
+    default_struct = {
+        "user": {
+            "active": bool,
+            "fname": str,
+            "invoice": {
+                "cost": str,
+                "address": {
+                    "street1": str,
+                    "street2": str,
+                    "city": str,
+                    "state": str,
+                    "zipcode": int,
+                },
+                "items": {
+                    "display_name": str,
+                    "sku": str,
+                },
             },
+        }
+    }
+
+    def test_single_query_var(self):
+        query = self.client.dict_to_query(
+            query_struct=self.default_struct,
             query_var={"user_id": "130897273"},
         )
         assert '(user_id: "130897273")' in query
 
     def test_multiple_query_var(self):
-        client = GqlClient("localhost")
-        query = client.dict_to_query(
-            query_struct={
-                "user": {
-                    "active": bool,
-                    "fname": str,
-                    "invoice": {
-                        "cost": str,
-                        "address": {
-                            "street1": str,
-                            "street2": str,
-                            "city": str,
-                            "state": str,
-                            "zipcode": int,
-                        },
-                        "items": {
-                            "display_name": str,
-                            "sku": str,
-                        },
-                    },
-                }
-            },
+        query = self.client.dict_to_query(
+            query_struct=self.default_struct,
             query_var={"user_id": "130897273", "active": True},
         )
         assert '(user_id: "130897273", active: true)' in query
 
     def test_output_structure_matches_query_struct(self):
-        client = GqlClient("localhost")
-        query = client.dict_to_query(
-            query_struct={
-                "user": {
-                    "active": bool,
-                    "fname": str,
-                    "invoice": {
-                        "cost": str,
-                        "address": {
-                            "street1": str,
-                            "street2": str,
-                            "city": str,
-                            "state": str,
-                            "zipcode": int,
-                        },
-                        "items": {
-                            "display_name": str,
-                            "sku": str,
-                        },
-                    },
-                }
-            },
+        query = self.client.dict_to_query(
+            query_struct=self.default_struct,
             query_var={"user_id": "130897273"},
         )
         assert query == (
