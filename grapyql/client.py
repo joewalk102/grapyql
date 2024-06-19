69fa99d590d6f03fe26b79e724d1dd9a73286fd7
Joe Walker <joewalk102@gmail.com>
2024-06-18 21:36:09 -0700

    Refine `dict_to_query()` so it can accept args at any level.
    Also, update requirements.txt

--- grapyql/client.py
+++ grapyql/client.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import requests
 
 
@@ -6,7 +8,8 @@ class GqlClient:
         self.host: str = host
         self.force_typing = force_typing
 
-    def _value_py_to_js(self, value):
+    @staticmethod
+    def _value_py_to_js(value):
         if isinstance(value, str):
             return f'"{value}"'
         elif isinstance(value, bool):
@@ -16,16 +19,16 @@ class GqlClient:
         raise ValueError(f"Value not supported: `{value}`")
 
     def dict_to_query(
-        self, query_struct: dict, query_var: dict = None, force_typing: bool = False
+        self, query_struct: dict, force_typing: bool = False, _start: bool = True
     ) -> str:
         """
         Convert a dictionary structure to a GraphQL Query
 
         Args:
             query_struct: Structure of the graphql query, expressed in dictionary form.
-            query_var: Search parameters for the graphql query.
             force_typing: Force the results to conform to the types provided. If they don't
               raise an error. If this option is disabled, the values are ignored.
+            _start: If this is the first call to the function, this should be True. (used for recursion)
 
         Returns: GraphQL query string is returned that can be sent to the GraphQL server.
 
@@ -54,33 +57,43 @@ class GqlClient:
           }
         }
         """
-        output = "{\n"
+        # first new line needs to have an outer dictionary to encapsulate the query.
+        output = "{\n" if _start else ""
         for k, v in query_struct.items():
+            if k == "__args__":
+                # args are not to be included in the query as a normal field.
+                continue
             if isinstance(v, dict):
-                if query_var is not None:
+                # If the value is a dictionary, then it is a nested field. But first check if it has
+                # query variables that need to be added to the current field.
+                if "__args__" in v:
                     # Add query variables to first row.
                     output += f"{k}("
-                    for qv_k, qv_v in query_var.items():
+                    qv_args = list()
+                    for qv_k, qv_v in v["__args__"].items():
                         # add every query_var
-                        output += f"{qv_k}: {self._value_py_to_js(qv_v)}"
-                        output += ", "
-                    # Remove space and comma from last one and continue with nested
-                    # fields in `v`
-                    output = output[:-2] + f") {self.dict_to_query(v)}"
+                        qv_args.append(f"{qv_k}: {self._value_py_to_js(qv_v)}")
+                    output += ", ".join(qv_args) + ") {\n  "
+                    # continue with nested fields in `v`
+                    output += self.dict_to_query(v, _start=False)
                     break
-                # else: if no query_var (must be nested call)
+                # Add any other nested fields to the next level.
                 output += f"{k} {self.dict_to_query(v)}"
             else:
+                # If the value is not a dictionary, then the key can just be added to the query.
                 output += k
             output += "\n"
         output = "\n  ".join(output.split("\n"))
-        output += "}"
-        if query_var is not None:
-            output = output[:-1] + "\n}"
+        # remove the last two characters from the output so the closing bracket is lined up.
+        output = output[:-2]
+        output += "}" if _start else "}\n"
         return output
 
     def query(
-        self, query: str = "", return_dict: bool = True, raise_on_error: bool = True
+        self,
+        query: Union[str, dict] = "",
+        return_dict: bool = True,
+        raise_on_error: bool = True,
     ):
         """
         Perform the specified query.
@@ -92,6 +105,8 @@ class GqlClient:
             raise_on_error: If there is an error in the request, either raise an error
               if this value is True, or return None if the value is False.
         """
+        if isinstance(query, dict):
+            query = self.dict_to_query(query)
         try:
             r = requests.post(self.host, json={"query": query})
         except requests.exceptions.RequestException as e:
--- test/test_client.py
+++ test/test_client.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from grapyql.client import GqlClient
 
 
@@ -5,9 +7,11 @@ class TestGqlClientDictToQuery:
     client = GqlClient("localhost")
     default_struct = {
         "user": {
+            "__args__": {"user_id": "$user_id"},
             "active": bool,
             "fname": str,
-            "invoice": {
+            "invoices": {
+                "__args__": {"limit": "$limit"},
                 "cost": str,
                 "address": {
                     "street1": str,
@@ -25,30 +29,25 @@ class TestGqlClientDictToQuery:
     }
 
     def test_single_query_var(self):
-        query = self.client.dict_to_query(
-            query_struct=self.default_struct,
-            query_var={"user_id": "130897273"},
-        )
+        struct = deepcopy(self.default_struct)
+        struct["user"]["__args__"] = {"user_id": "130897273"}
+        query = self.client.dict_to_query(query_struct=struct)
         assert '(user_id: "130897273")' in query
 
     def test_multiple_query_var(self):
-        query = self.client.dict_to_query(
-            query_struct=self.default_struct,
-            query_var={"user_id": "130897273", "active": True},
-        )
+        struct = deepcopy(self.default_struct)
+        struct["user"]["__args__"] = {"user_id": "130897273", "active": True}
+        query = self.client.dict_to_query(query_struct=struct)
         assert '(user_id: "130897273", active: true)' in query
 
     def test_output_structure_matches_query_struct(self):
-        query = self.client.dict_to_query(
-            query_struct=self.default_struct,
-            query_var={"user_id": "130897273"},
-        )
+        query = self.client.dict_to_query(query_struct=self.default_struct)
         assert query == (
             "{\n"
-            '  user(user_id: "130897273") {\n'
+            '  user(user_id: "$user_id") {\n'
             "    active\n"
             "    fname\n"
-            "    invoice {\n"
+            '    invoices(limit: "$limit") {\n'
             "      cost\n"
             "      address {\n"
             "        street1\n"
@@ -56,12 +55,12 @@ class TestGqlClientDictToQuery:
             "        city\n"
             "        state\n"
             "        zipcode\n"
-            "        }\n"
+            "      }\n"
             "      items {\n"
             "        display_name\n"
             "        sku\n"
-            "        }\n"
             "      }\n"
             "    }\n"
+            "  }\n"
             "}"
         )
